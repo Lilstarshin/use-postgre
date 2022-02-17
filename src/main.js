1be7c6d8ba8f2198d75d16409dd2aa59c730bf4c
lilstar <lilstar.shin@gmail.com>
2022-02-17 16:04:28 +0900

    First main.js commit

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
 // @ts-check
+
+const { Client } = require('pg');
+const client = new Client();
+async function main() {
+  await client.connect();
+
+  const res = await client.query('SELECT $1::text as message', [
+    'Hello world!',
+  ]);
+  console.log(res.rows[0].message); // Hello world!
+  await client.end();
+}
